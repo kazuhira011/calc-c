6aad821d960f9fb149223f45486c2a5c9ff204ea
Kazu <113638675+kazuhira011@users.noreply.github.com>
2023-01-05 13:35:07 -0300

    Add files via upload

--- calculadora.c
+++ calculadora.c
@@ -21,30 +21,28 @@ int main(){
 
     if (respostauser == 1){
         num1 = num1 + num2;
-        printf("O resultado da soma %c: %f \n", 130, num1);
+        printf("O resultado da soma %c: %.2f \n", 130, num1);
+        system("pause");
     }
 
     if (respostauser == 2){
         num1 = num1 - num2;
-        printf("O resultado da subtra%c%co %c: %f \n", 135, 198, 130, num1);
+        printf("O resultado da subtra%c%co %c: %.2f \n", 135, 198, 130, num1);
+        system("pause");
     }
 
     if (respostauser == 3){
         num1 = num1 * num2;
-        printf("O resultado da multiplica%c%co %c: %f \n", 135, 198, 130, num1);
+        printf("O resultado da multiplica%c%co %c: %.2f \n", 135, 198, 130, num1);
+        system("pause");
     }
 
     if (respostauser == 4){
         num1 = num1 / num2;
         printf("O resultado da divis%co %c: %.2f \n", 198, 130, num1);
+        system("pause");
     }
-        
-    else{
-        printf("Op%c%co inv%clida! \n", 135, 198, 160);
     }
-
-    system("pause");
-
-    }
+    
     return 0;
 }
